d7084a09c6ac6dea7080e2780fff795bf4441966
안병준 <neostgeart@gmail.com>
2021-12-31 15:48:30 +0900

    Update

--- redux_react/src/components/App.js
+++ redux_react/src/components/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from '../routes/Home';
-import Detail from '../routes/Detail';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path = "/" element = {<Home />}></Route>
-        <Route path = '/:id' element = {<Detail />}></Route>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/:id" element={<Detail />}></Route>
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default App;
